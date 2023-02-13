35995e8c23037ada83cc23d4c44767d4785757a5
Nashruddin Amin <nashruddin.amin@gmail.com>
2023-02-13 17:11:52 +0700

    Set pagination settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -155,4 +155,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# Django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 12
+}
+
+
 SAMPLE_QUOTE = env('SAMPLE_QUOTE')
